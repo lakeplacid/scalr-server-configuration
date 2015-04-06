0642b5976168cd06b32b1ab369b14b777848f4c9
Thomas Orozco <thomas@orozco.fr>
2015-04-06 14:15:54 +0200

    [external-mysql] Add MySQL host / port config

--- external-mysql/scalr-server.rb
+++ external-mysql/scalr-server.rb
@@ -5,3 +5,11 @@
 
 # Disable all services. Everything else will be re-enabled in scalr-server-local.rb
 enable_all false
+
+
+# Use separate MySQL
+app[:mysql_scalr_host] = '127.0.0.1'      # Host for the Scalr MySQL database.
+app[:mysql_scalr_port] = 3306             # Port for the Scalr MySQL database. (note that by default the installer sets up MySQL to listen on port 6280).
+
+app[:mysql_analytics_host] = '127.0.0.1'  # Host for the Analytics MySQL database.
+app[:mysql_analytics_port] = 3306         # Port for the Analytics MySQL database (same as above).
