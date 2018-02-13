d10231d8777ecd5b4063c20c418df91c5cee447a
Ryan <ryan.fee625@gmail.com>
2018-02-13 14:23:52 -0500

    Update scalr-server.rb

--- 5-server-ha/scalr-server.rb
+++ 5-server-ha/scalr-server.rb
@@ -47,8 +47,6 @@ app[:mysql_analytics_port] = 3306
 
 # Memcached Servers
 app[:memcached_servers] = [APP_SERVER_1 + ':' + MEMCACHED_PORT, APP_SERVER_2 + ':' + MEMCACHED_PORT]
-# Only needs to be done prior to 7.7.2: Disable sasl
-memcached[:enable_sasl] = false
 
 # Look for the app and graphics locally as well
 proxy[:app_upstreams] = ['127.0.0.1:6000']
@@ -76,15 +74,12 @@ memcached[:bind_host] = '0.0.0.0'
 memcached[:bind_port] = 11211
 
 # Scalr Web/AMQP Host
-#routing[:endpoint_host] = proxy_host
 app[:influxdb_host] = INFLUXDB_SERVER
 influxdb[:http_bind_host] = '0.0.0.0'
 proxy[:amqps_bind_port] = 5671
 app[:rabbitmq_host] = INFLUXDB_SERVER
 rabbitmq[:bind_host] = '0.0.0.0'
 rabbitmq[:mgmt_bind_host] = '0.0.0.0'
-
-# Disable all services by default
 
 app[:configuration] = {
   :scalr => {
